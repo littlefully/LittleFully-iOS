d3190e3df72389f726383c3b3061aaa06d7e3397
Nico Prananta <nicnocquee@yahoo.com>
2014-03-07 11:12:51 +0900

    photo tests. #11

---
+++ LittleFullyTests/ModelTests/LFYPhotoTests.m
@@ -0,0 +1,50 @@
+//
+//  LFYPhotoTests.m
+//  LittleFully
+//
+//  Created by Nico Prananta on 3/7/14.
+//  Copyright (c) 2014 Little Fully. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "XCTestCase+Additionals.h"
+
+#import <Expecta.h>
+
+#import "LFYPhoto.h"
+
+@interface LFYPhotoTests : XCTestCase
+
+@end
+
+@implementation LFYPhotoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testSerialization
+{
+    NSDictionary *photoDictionary = [self objectFromJSONFile:@"Photo"];
+    NSError *error;
+    LFYPhoto *photo = [MTLJSONAdapter modelOfClass:[LFYPhoto class] fromJSONDictionary:photoDictionary error:&error];
+    
+    expect(error).to.beNil;
+    expect(photo).toNot.beNil;
+    expect(photo.width).to.equal(photoDictionary[@"width"]);
+    expect(photo.height).to.equal(photoDictionary[@"height"]);
+    expect(photo.objectId).to.equal(photoDictionary[@"id"]);
+    expect(photo.originalURL.absoluteString).to.equal(photoDictionary[@"originalURL"]);
+    expect(photo.thumbnail200x200URL.absoluteString).to.equal(photoDictionary[@"thumbnail200x200URL"]);
+}
+
+@end
